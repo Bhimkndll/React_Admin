6d736763f0d795b746a367cd4ffa3228066fc7e9
Bhimkndll <vimkndll12@gmail.com>
2021-02-05 06:54:47 +0545

    config set

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,14 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" type="text/css" href="{{secure asset('assets/css/bootstrap.min.css')}}">
-<link rel="stylesheet" type="text/css" href="{{secure asset('assets/css/font-awesome.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.min.css')}}">
 
-<link rel="stylesheet" type="text/css" href="{{secure asset('assets/css/slick.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/slick.css')}}">
 
-<link rel="stylesheet" type="text/css" href="{{secure asset('assets/css/style.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
 
-<link rel="stylesheet" type="text/css" href="{{secure asset('assets/css/ads.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/ads.css')}}">
 
 </head>
 
