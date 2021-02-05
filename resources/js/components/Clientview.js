ac8f9f328fd1e91640f9a40d3bc6532e75eec4e9
Bhimkndll <vimkndll12@gmail.com>
2021-02-05 07:55:32 +0545

    config set

--- resources/js/components/Clientview.js
+++ resources/js/components/Clientview.js
@@ -7,7 +7,6 @@ import Fclient from './client/Fclient';
 import PopularPost from './client/PopularPost';
 import CoverflowEffect from './client/CoverflowEffect';
 import Subpost from './client/subclient/Subpost';
-import Footers from './client/Footers';
 import Navbar from './client/Navbar';
 
 class Clientview extends React.Component {
