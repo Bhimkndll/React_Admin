fd270738dd304b40d294e22f6824fb83e5eaa3ac
Bhimkndll <vimkndll12@gmail.com>
2021-02-11 22:02:39 +0545

    initial

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,6 @@ use Laravel\Passport\Http\Controllers\AccessTokenController;
     Route::post('signup', 'Auth\ApiAuthController@signup');
   Route::get('signup/activate/{token}', 'Auth\ApiAuthController@signupActivate');
     Route::group([
-      'middleware' => 'auth:sanctum'
     ], function() {
 
 
@@ -44,8 +43,7 @@ use Laravel\Passport\Http\Controllers\AccessTokenController;
     });
     });
      Route::group([
-/*    'namespace' => 'Auth',
-*/    'middleware' => 'auth:sanctum',
+
     'prefix' => 'password'
 ], function () {
     Route::post('create', 'PasswordResetController@create');
@@ -55,23 +53,21 @@ use Laravel\Passport\Http\Controllers\AccessTokenController;
 });
 
  Route::group([
-   'middleware' => 'auth:sanctum'
 ], function () {
 
 Route::get('/bhim','CrudController@index')->name('cruddisply');
 
-Route::post('/contact','CrudController@store')->name('crudestore')->middleware('auth:api');
+Route::post('/contact','CrudController@store')->name('crudestore');
 Route::get('edit/{id}','CrudController@edit')->name('crud.display');
 Route::post('update/{id}','CrudController@update')->name('updatecrud');
 Route::get('/delete/{id}','CrudController@delete')->name('cruddelete');
 
 
-Route::post('/category/store','CategoryController@store')->name('crudeestore')->middleware('auth:sanctum');
+Route::post('/category/store','CategoryController@store')->name('crudeestore')
 Route::get('/category/edit/{id}','CategoryController@edit')->name('crud.display');
 Route::post('category/save/{id}','CategoryController@save')->name('updatecrud');
 Route::get('category/delete/{id}','CategoryController@delete')->name('cruddelete');
-Route::get('/category','CategoryController@index')->name('cateview')->middleware('auth:sanctum');
-
+Route::get('/category','CategoryController@index')->name('cateview');
 
 Route::post('/tag/store','Api\TagController@store')->name('crudeestore');
 Route::get('/tag/edit/{id}','Api\TagController@edit')->name('crud.display');
